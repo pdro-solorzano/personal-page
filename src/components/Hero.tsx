135b081b93a72fc791e67681989a832dd175aa1e
Pedro Solorzano <pedrosolorzano@MacBookPro.attlocal.net>
2025-07-07 15:29:09 -0500

    Hero component finished, mobile and bigscreen adapted

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,89 @@
+import { Container, Grid, Typography } from "@mui/material";
+import { SocialMediaBar } from "./SocialMediaBar";
+
 function Hero() {
-    return ()
+  return (
+    <Container
+      maxWidth="xl"
+      disableGutters
+      sx={{
+        backgroundImage:
+          "radial-gradient(circle,rgba(42, 46, 56, 1) 0%, rgba(8, 14, 24, 1) 100%) ",
+        height: "100vh",
+      }}
+    >
+      <Grid
+        container
+        columnSpacing={1}
+        rowSpacing={{ xs: 8, md: 2 }}
+        alignItems="center"
+        maxWidth="lg"
+        sx={{
+          mx: { xs: 0, md: "auto" },
+          px: { xs: 2.5, md: 10 },
+          pt: { xs: 8, md: 15 },
+        }}
+      >
+        <Grid size={{ xs: 12, md: 5 }}>
+          <Typography variant="h3" sx={{ typography: { xs: "h4", md: "h3" } }}>
+            My name is{" "}
+            <Typography
+              variant="h3"
+              component="span"
+              color="secondary"
+              fontWeight="medium"
+              sx={{
+                textDecoration: "underline",
+                typography: { xs: "h4", md: "h3" },
+              }}
+            >
+              Pedro&nbsp;Solorzano
+            </Typography>{" "}
+            and I'm{" "}
+            <Typography
+              variant="h3"
+              component="span"
+              color="secondary"
+              fontWeight="medium"
+              sx={{
+                textDecoration: "underline",
+                typography: { xs: "h4", md: "h3" },
+              }}
+            >
+              Web&nbsp;Developer
+            </Typography>
+          </Typography>
+        </Grid>
+        <Grid
+          height="65vh"
+          size={{ xs: 0, md: 2 }}
+          sx={{ display: { xs: "none", md: "flex" } }}
+        ></Grid>
+        <Grid size={{ xs: 12, md: 5 }}>
+          <Typography variant="body1">
+            I love building clean, scalable code that solves real user problems.
+            With a strong focus on modern frontend development using{" "}
+            <Typography variant="body1" component="span" color="secondary">
+              React
+            </Typography>{" "}
+            and{" "}
+            <Typography variant="body1" component="span" color="secondary">
+              TypeScript
+            </Typography>
+            , I bring quality into the product from the first commit—automating
+            tests and integrating{" "}
+            <Typography variant="body1" component="span" color="secondary">
+              CI/CD pipelines
+            </Typography>{" "}
+            to move fast with confidence.
+          </Typography>
+        </Grid>
+        <Grid size={{ xs: 12 }}>
+          <SocialMediaBar />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export { Hero }
+export { Hero };
